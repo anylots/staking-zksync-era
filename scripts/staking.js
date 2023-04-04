c2b06e4a32d7c5fa2c084588187457894e44aee2
anylots <842567494@qq.com>
2023-04-04 17:24:32 +0800

    staking

--- scripts/staking.js
+++ scripts/staking.js
@@ -1,7 +1,9 @@
-const aleo_address = "0x8bcEEE23a628C8bc98df37941Ad24bD950F1853F";
-const Token_Artifact = require("../artifacts/contracts/Token.sol/AleoToken.json");
-const stakingHub_address = "0x00BF3DEeEeD0F7cdDdEe197574B82b845013fDF7";
-const StakingHubArtifact = require("../artifacts/contracts/StakingHub.sol/StakingHub.json");
+const aleo_address = "0x90D3d849c856713F21E824E943C7C83792c51381";
+const Token_Artifact = require("../artifacts-zk/contracts/Token.sol/RewardToken.json");
+const stakingHub_address = "0x99Dd8146271A66a49DB8A592A7C94BEeb3C42e06";
+const StakingHubArtifact = require("../artifacts-zk/contracts/StakingHub.sol/StakingHub.json");
+const { ethers } = require('ethers');
+
 
 const overrides = {
   gasLimit: 15000000,
@@ -12,56 +14,48 @@ const overrides = {
 // yours, or create new ones.
 async function main() {
   ///Prepare deployer
-  let privateKey = "0x537682a041dc2a904573b6045bfbc9442940868b6aabaaa64bb4036677feb69a";
+  let privateKey = "0x433200fedb2386cd4fa915ce4c6777729534458339c29d110db4124997715fe3";
   let customHttpProvider = new ethers.providers.JsonRpcProvider(
-    "http://47.242.179.164:9933"
+    "https://testnet.era.zksync.dev"
   );
   const signer = new ethers.Wallet(privateKey, customHttpProvider);
   console.log(signer.address);
 
   ///deposit
   let token = new ethers.Contract(aleo_address, Token_Artifact.abi, signer);
+  //999999989900
   console.log("approve...");
-  await token.approve(stakingHub_address, ethers.utils.parseUnits("1", 6), overrides);
+  // await token.approve(stakingHub_address, ethers.utils.parseUnits("100", 6), overrides);
 
   let StakingHub = new ethers.Contract(
     stakingHub_address,
     StakingHubArtifact.abi,
     signer
   );
-  let deposit = await StakingHub.deposit(
-    ethers.utils.parseUnits("1", 6),
-    signer.address,
-    overrides
-  );
-  console.log("deposit:" + deposit.hash);
+  // let deposit = await StakingHub.deposit(
+  //   ethers.utils.parseUnits("1", 6),
+  //   signer.address,
+  //   overrides
+  // );
+  // console.log("deposit:" + deposit.hash);
 
-  await new Promise((resolve, reject) => {
-    setTimeout(function () {
-      resolve('time')
-    }, 3000)
-  })
-  let receipt = await customHttpProvider.getTransactionReceipt(deposit.hash);
+  // await new Promise((resolve, reject) => {
+  //   setTimeout(function () {
+  //     resolve('time')
+  //   }, 3000)
+  // })
+  // let receipt = await customHttpProvider.getTransactionReceipt(deposit.hash);
   // console.log(receipt);
 
   ///reviewAssets
   let reviewAssets = await StakingHub.reviewAssets(signer.address);
   console.log("user's staking balance: " + reviewAssets);
 
-  // for (let i = 0; i < 10; i++) {
-  //   await new Promise((resolve, reject) => {
-  //     setTimeout(function () {
-  //       resolve('time')
-  //     }, 2000)
-  //   })
-  //   let staking_reward = await StakingHub.reviewReward(signer.address);
-  //   console.log("user's staking reward: " + staking_reward);
-  // }
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
